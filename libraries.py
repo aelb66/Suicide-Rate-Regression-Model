ee42c385e1c38c3186738a1fafd9b596a9422c20
aelb66 <75398560+aelb66@users.noreply.github.com>
2021-07-12 19:28:26 +1000

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,7 @@
+library(tidyverse) #data manipulation
+library(dplyr) #data manipulation
+import pandas as pd #data manipulation
+import matplotlib.pyplot as plt #visualisation
+import seaborn as sns #visualisation
+import numpy as np #data transformations
+import sklearn # machine learning
