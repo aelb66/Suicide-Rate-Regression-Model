1ffd6f420607646a04ba4260a5d586e82df60dad
aelb66 <75398560+aelb66@users.noreply.github.com>
2021-07-12 20:25:46 +1000

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -1,7 +1,19 @@
-library(tidyverse) #data manipulation
-library(dplyr) #data manipulation
 import pandas as pd #data manipulation
+import numpy as np #data transformations
+
 import matplotlib.pyplot as plt #visualisation
 import seaborn as sns #visualisation
-import numpy as np #data transformations
+
 import sklearn # machine learning
+from sklearn.model_selection import train_test_split #train and test split
+from sklearn.linear_model import LinearRegression #Linear Regression
+from sklearn.ensemble import RandomForestRegressor #Random Forest
+from sklearn.tree import DecisionTreeRegressor #Decision Tree
+from sklearn.metrics import mean_squared_error, r2_score #performance metrics
+from sklearn.model_selection import GridSearchCV #cross-validation
+
+import statsmodels.api as sm #one-way ANOVA
+from statsmodels.formula.api import ols  #importing stats package
+import scipy.stats as stats #statistics
+from sklearn.preprocessing import MinMaxScaler #normalisations
+from sklearn.preprocessing import StandardScaler #normalisations
